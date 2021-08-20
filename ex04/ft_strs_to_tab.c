9260d8a292716b9ea194ec8a63c8d65c0ff40345
kazukitkh <kazuki.tkh@gmail.com>
2021-08-20 09:25:26 +0900

    k

---
+++ ex04/ft_strs_to_tab.c
@@ -0,0 +1,67 @@
+//#include <stdio.h>
+#include "ft_stock_str.h"
+#include <stdlib.h>
+
+void	ft_str_to_copy(t_stock_str *c, char *d, int i, int j)
+{
+	d[j] = 0;
+	d = c[i].str;
+	c[i].copy = d;
+	c[i].size = j;
+}
+
+int	ft_strlen(char *c)
+{
+	int		i;
+
+	i = 0;
+	while (c[i])
+		i++;
+	return (i);
+}
+
+struct s_stock_str	*ft_strs_to_tab(int ac, char **av)
+{
+	int			i;
+	int			j;
+	t_stock_str	*c;
+	char		*d;
+
+	i = 0;
+	c = (t_stock_str *)malloc(sizeof(t_stock_str) * (ac + 1));
+	if (!c)
+		return ((void *)0);
+	c[ac].str = 0;
+	c[ac].size = 0;
+	c[ac].copy = 0;
+	while (i < ac)
+	{
+		c[i].str = av[i];
+		j = ft_strlen(c[i].str);
+		d = (char *)malloc(sizeof(char) * (j + 1));
+		ft_str_to_copy(c, d, i, j);
+		free(d);
+		i++;
+	}
+	return ((t_stock_str *)c);
+}
+
+/*int	main(int argc, char *argv[])
+{
+	t_stock_str *tmp;
+	int	i;
+
+	i = 0;
+	tmp = (t_stock_str *)malloc(sizeof(t_stock_str) * (argc));
+	tmp[argc - 1].size = 0;
+	tmp[argc - 1].str = 0;
+	tmp[argc - 1].copy = 0;
+	tmp = ft_strs_to_tab(argc - 1, argv + 1);
+	while (tmp[i].size)
+	{
+		printf("%d, %s, %s", tmp[i].size, tmp[i].str, tmp[i].copy);
+		putchar('\n');
+		i++;
+	}
+	return (0);
+}*?
---
+++ ex05/ft_show_tab.c
@@ -0,0 +1,54 @@
+#include "ft_stock_str.h"
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *c)
+{
+	int	j;
+
+	j = 0;
+	while (c + j++);
+		ft_putchar(c[j - 1]);
+	ft_putchar('\n');
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		if (nb == -2147483648)
+		{
+			nb = -147483648;
+			ft_putchar('2');
+		}
+		nb = -nb;
+	}
+	if (nb >= 10)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+		ft_putchar(nb + 48);
+}
+
+void	ft_show_tab(struct s_stock_str *par)
+{
+	t_stock_str *tmp;
+	int	i;
+
+	i = 0;
+	while (par[i].size)
+	{
+		ft_putstr(par[i].str);
+		ft_putnbr(par[i].size);
+		ft_putchar('\n');
+		ft_putstr(par[i].copy);
+		i++;
+	}
+}
